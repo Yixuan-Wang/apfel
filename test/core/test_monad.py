55b49370efe2c59ec0d5811741bc8016986eeb4b
Yixuan-Wang <tom-yx_wang@outlook.com>
2025-03-05 00:37:06 +0800

    feat(monad): add monadic interfaces and impls
    Squashed commit of the following:
    
    commit 2f2ef7501f5ff2d6fa5a818b091adf92540abe7a
    Author: Yixuan-Wang <tom-yx_wang@outlook.com>
    Date:   Tue Mar 4 15:43:41 2025 +0800
    
        feat(monad): impl monad for std types
    
    commit 1900257ff0dec35df3717753a1e358e4d69d91b5
    Author: Yixuan-Wang <tom-yx_wang@outlook.com>
    Date:   Tue Mar 4 15:43:17 2025 +0800
    
        feat(dispatch): add add_impl
    
    commit deb3106ed45325f040d795f6a376bdd9541236d8
    Author: Yixuan-Wang <tom-yx_wang@outlook.com>
    Date:   Wed Feb 26 03:06:00 2025 +0800
    
        feat(monad): implement for basic types
    
        - `list`, `tuple`, `set` and `dict`
    
    commit 56468eb945a839c56a0890afe659c4702c32f5d8
    Author: Yixuan-Wang <tom-yx_wang@outlook.com>
    Date:   Wed Feb 26 02:09:00 2025 +0800
    
        fix(dispatch): missing ABC facility
    
    commit 1d4b2b24c51709b4118a4d17ff75c073b24b292c
    Author: Yixuan-Wang <tom-yx_wang@outlook.com>
    Date:   Wed Feb 26 01:16:24 2025 +0800
    
        refactor(monad): promote monad to package
    
    commit 227a343becda22a48cccc8a78838843f0ed5464f
    Author: Yixuan-Wang <tom-yx_wang@outlook.com>
    Date:   Tue Feb 11 12:02:16 2025 +0800
    
        fix(monad): typing
    
    commit 9f854e49a1f6a47babe5461aaac2b1eae77926ed
    Author: Yixuan-Wang <tom-yx_wang@outlook.com>
    Date:   Thu Feb 6 20:20:18 2025 +0800
    
        feat!(monad): single dispatch for monad
    
    commit c2a22b00a41330ad5853ab7011b0ebd134dad4d7
    Author: Yixuan-Wang <tom-yx_wang@outlook.com>
    Date:   Thu Feb 6 17:51:01 2025 +0800
    
        chore: add ipdb
    
    commit b11d3fd83a216fd5041bda4865c1b71aa9012b1d
    Author: Yixuan-Wang <tom-yx_wang@outlook.com>
    Date:   Thu Feb 6 17:07:39 2025 +0800
    
        feat!(dispatch): refined dispatch
    
        - better interface names
        - single dispatch on classmethod and staticmethod
          now use `[]` syntax for extra argument instead
          of changing the signature of the abstractmethod
    
    commit 12ee6a60d26318cf7d6a8e1ed0b87ad79797816e
    Author: Yixuan-Wang <tom-yx_wang@outlook.com>
    Date:   Thu Feb 6 16:46:43 2025 +0800
    
        feat(function): add identity, todo and unimpl
    
    commit 26e3f98fec487eb475865f4b60cbd9214368858b
    Author: Yixuan-Wang <tom-yx_wang@outlook.com>
    Date:   Tue Feb 4 15:28:55 2025 +0800
    
        feat(dispatch): dispatch and impl decorators
    
    commit 24c6fe34a55d283fc24416d8bea382cb7c948fde
    Author: Yixuan-Wang <tom-yx_wang@outlook.com>
    Date:   Tue Feb 4 00:39:16 2025 +0800
    
        fix(dispatch): fix dispatch func
    
    commit fb81411c6090484375a0c5f75779d243c72c2c21
    Author: Yixuan-Wang <tom-yx_wang@outlook.com>
    Date:   Mon Feb 3 23:32:38 2025 +0800
    
        feat(dispatch): move annotation to stub file
    
    commit e3823d8e456b93f98b0bcb253eda93788ca09e11
    Author: Yixuan-Wang <tom-yx_wang@outlook.com>
    Date:   Mon Feb 3 22:49:01 2025 +0800
    
        feat(dispatch): add basic interface of dispatch
    
    commit 52ee2c0d05981da9efe5bd84a875e2ac5c34fb82
    Author: Yixuan-Wang <tom-yx_wang@outlook.com>
    Date:   Thu Oct 10 01:53:15 2024 -0500
    
        feat(core): add monad
    
        Introduced the Haskell Monad typeclass hierarchy
        - Functor
        - Applicative
        - Monad

---
+++ test/core/test_monad.py
@@ -0,0 +1,134 @@
+# type: ignore
+
+from __future__ import annotations
+
+from apfel.core.monad import Functor, Applicative, Monad
+from apfel.core.dispatch import impl
+
+class Maybe[T](Monad):
+    value: T
+    is_nothing: bool
+
+    def __init__(self, value: T | None = None):
+        if value is None:
+            self.is_nothing = True
+        else:
+            self.value = value
+            self.is_nothing = False
+
+    @classmethod
+    def pure(cls, x):
+        return cls(x)
+    
+    def bind(self, f):
+        if self.is_nothing:
+            return self
+        return f(self.value)
+    
+    def __eq__(self, other):
+        return isinstance(other, Maybe) and self.is_nothing == other.is_nothing and self.value == other.value
+
+
+def test_monad_maybe():
+    assert Maybe.__abstractmethods__ == set()
+
+    maybe = Maybe(42)
+    
+    assert maybe.map(lambda x: x + 1) == Maybe(43) # type: ignore
+    assert maybe.apply(Maybe(lambda x: x + 1)) == Maybe(43) # type: ignore
+    assert maybe.bind(lambda x: Maybe(x + 1)) == Maybe(43) # type: ignore
+
+def test_monad_impl_list():
+    assert issubclass(list, Functor)
+    assert issubclass(list, Applicative)
+    assert issubclass(list, Monad)
+
+    lst = [1, 2, 3]
+
+    import operator
+    from functools import partial
+
+    def add(x):
+        return partial(operator.add, x)
+
+    assert Functor.map(lst, add(1)) == [2, 3, 4]
+    assert Applicative.pure[list](42) == [42]
+    assert Applicative.apply(lst, [add(1), add(2)]) == [2, 3, 4, 3, 4, 5]
+    assert Monad.bind(lst, lambda x: [x, x + 1]) == [1, 2, 2, 3, 3, 4]
+
+def test_monad_impl_tuple():
+    assert issubclass(tuple, Functor)
+    assert issubclass(tuple, Applicative)
+    assert issubclass(tuple, Monad)
+
+    tpl = (1, 2, 3)
+
+    import operator
+    from functools import partial
+
+    def add(x):
+        return partial(operator.add, x)
+
+    assert Functor.map(tpl, add(1)) == (2, 3, 4)
+    assert Applicative.pure[tuple](42) == (42,)
+    assert Applicative.apply(tpl, (add(1), add(2))) == (2, 3, 4, 3, 4, 5)
+    assert Monad.bind(tpl, lambda x: (x, x + 1)) == (1, 2, 2, 3, 3, 4)
+
+def test_monad_impl_set():
+    assert issubclass(set, Functor)
+    assert issubclass(set, Applicative)
+    assert issubclass(set, Monad)
+
+    st = {1, 2, 3}
+
+    import operator
+    from functools import partial
+
+    def add(x):
+        return partial(operator.add, x)
+
+    assert Functor.map(st, add(1)) == {2, 3, 4}
+    assert Applicative.pure[set](42) == {42}
+    assert Applicative.apply(st, {add(1), add(2)}) == {2, 3, 4, 5}
+    assert Monad.bind(st, lambda x: {x, x + 1}) == {1, 2, 3, 4}
+
+def test_monad_impl_dict():
+    assert issubclass(dict, Functor)
+    assert not issubclass(dict, Applicative)
+    assert not issubclass(dict, Monad)
+
+    dct = {1: 2, 3: 4}
+
+    import operator
+    from functools import partial
+
+    def add(x):
+        return partial(operator.add, x)
+
+    assert Functor.map(dct, add(1)) == {1: 3, 3: 5}
+
+def test_monad_impl_function():
+    from types import FunctionType
+
+    assert issubclass(FunctionType, Functor)
+    assert issubclass(FunctionType, Applicative)
+    assert issubclass(FunctionType, Monad)
+
+    def f(x):
+        return x + 1
+    
+    def add(x):
+        return lambda y: f"{x}{y}"
+
+    assert Functor.map(f, add(1))(42) == (lambda x: add(1)(x + 1))(42)
+    assert Applicative.pure[FunctionType](42)() == 42
+    assert Applicative.apply(f, lambda x: add(x))(42) == (lambda x: add(x)(x + 1))(42) == "4243"
+    assert Monad.bind(f, lambda x: add(x))(42) == (lambda x: add(x + 1)(x))(42) == "4342"
+
+def test_monad_impl_function_ext():
+    """
+    Test the implementation on BuiltinFunctionType and types
+    """
+
+    assert Functor.map(map, list)(lambda x: x + 1, [1, 2, 3, 4]) == list(map(lambda x: x + 1, [1, 2, 3, 4]))
+    assert Functor.map(str, list)(1) == ["1"]
