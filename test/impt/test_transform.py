0986ac645b8012d7158f76f89a96fddc44dcf1dd
Yixuan-Wang <tom-yx_wang@outlook.com>
2025-03-10 14:09:04 +0800

    test(impt-transform): check module cache
    previously typing_extensions is not imported in the library,
    but now it is, should remove its cache before testing
    transform

--- test/impt/test_transform.py
+++ test/impt/test_transform.py
@@ -1,6 +1,10 @@
 def test_transform():
     from apfel.impt.transform import register_transform_hook
     import importlib
+    from sys import modules
+
+    if "typing_extensions" in modules:
+        del modules["typing_extensions"]
 
     def hook(source: str) -> str:
         return source + "\nHI = 'Hello, World!'\n"
