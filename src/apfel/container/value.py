ee7cff54240dfecc72a1375d286effa713e5cfb1
Yixuan-Wang <tom-yx_wang@outlook.com>
2025-05-12 18:25:02 +0800

    feat(value): remove mutate in favor of run

--- src/apfel/container/value.py
+++ src/apfel/container/value.py
@@ -5,12 +5,13 @@ designed to aid chained method calls.
 
 from apfel.core.monad import Functor
 
+
 class Value(Functor):
-    __slots__ = ("_value", )
+    __slots__ = ("_value",)
 
     def __init__(self, value):
         self._value = value
-        
+
         if hasattr(value, "__doc__"):
             self.__doc__ = value.__doc__
 
@@ -38,36 +39,40 @@ class Value(Functor):
         """
         return Value(func(self._value))
 
-    def mutate(self, func):
-        """
-        Call a function over the inner value, ignore the return value,
-        and return the original container.
-
-        The behavior is same as [`Value.tap`][apfel.container.value.Value.tap],
-        just that the inner value is pragmatically allowed to
-        be mutated in place.
-        """
-
-        func(self._value)
-        return self
-
     def pipe(self, func):
         """
-        Run the function to process the inner value, and place the
-        result back to the container.
+        Run a function to process the inner value.
+        If a result is produced, the result will be put back to the container.
+        Otherwise, the original reference within the container will be kept.
+
 
         Warning:
             Compare to [`Value.map`][apfel.container.value.Value.map], this method
             mutates the container in place. No new container is created.
+            Therefore, the function must return a result of the same type as the
+            previous inner type of the container, or return `None`.
 
         Args:
-            func (Callable[[T], R]): A function to mutate the value.
+            func (Callable[[T], T] | Callable[[T], None]): A function to process the value.
 
         Returns:
-            (Value[R]): The mutated container itself.
+            (Value[T]): The mutated container itself.
         """
-        self._value = func(self._value)
+        val = func(self._value)
+        self._value = val if val is not None else self._value
         return self
+
+    def run(self, func):
+        """
+        Run a function to process the inner value, and return the result.
+
+        Args:
+            func (Callable[[T], R]): A function to process the value.
+
+        Returns:
+            (R): The result of the function.
+        """
+        return func(self._value)
 
     def tap(self, func):
         """
@@ -77,10 +82,9 @@ class Value(Functor):
         See also [`also`](https://kotlinlang.org/docs/scope-functions.html#also){ .ref .kt }.
 
         Warning:
-            Pragmatically, the function shall not mutate the inner value.
-            If mutating the inner value is desired, see [`Value.mutate`][apfel.container.value.Value.mutate].
+            Pragmatically, the function shouldn't mutate the inner value.
+            Use the [`Value.pipe`][apfel.container.value.Value.pipe] method instead.
         """
 
         func(self._value)
         return self
-
